4523c94d33610e8084d995f3d293cb1ae38201be
Sushil Pant <sushilpant83@gamil.com>
2025-07-26 21:41:59 +0530

    me1display

---
+++ ME1display.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+   
+    string name = "XYZ ZZZ XXX";
+    int rollNo = 12025;
+    int semester = 3;
+    string institution = "Amritsar Group of Colleges";
+
+    cout << "Name- " << name << endl;
+    cout << "Roll no.- " << rollNo << endl;
+    cout << "Semester-" << semester << endl;
+    cout << "Institution- " << institution << endl;
+
+    return 0;
+}
